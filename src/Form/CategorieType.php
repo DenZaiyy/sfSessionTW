8042e84617dc2a51c41efac24f36cae8123254a7
DenZaiyy <denzaiyy@outlook.fr>
2023-04-25 18:03:06 +0200

    update formtype

--- src/Form/CategorieType.php
+++ src/Form/CategorieType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\UX\Dropzone\Form\DropzoneType;
 
 class CategorieType extends AbstractType
 {
@@ -15,6 +17,22 @@ class CategorieType extends AbstractType
         $builder
             ->add('label', null, [
                 'attr' => ['class' => 'bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5']
+            ])
+            ->add('image', DropzoneType::class, [
+                'label' => 'Avatar',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => "1024k",
+                        'mimeTypes' => [
+                            'image/jpg',
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image file'
+                    ])
+                ],
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'bg-slate-900 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 w-full mt-5']
